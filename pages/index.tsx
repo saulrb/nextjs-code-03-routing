444c39cf37d448650492b12fdbbb7be7f8a02bb6
saul rosales <saul.rosalesb@gmail.com>
2024-03-24 18:55:27 -0400

    Added a Head tag component for metadata:

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
+import Head from 'next/head'
+
 import { getFeaturedEvents } from '../helpers/api-util'
 import EventList from '../components/events/eventList'
-import Event from '../data-model/EventInt'
 
 export default function HomePage(props: any) {
   return (
     <div>
+      <Head>
+        <title> Events application</title>
+        <meta name="description" content="Showing the best events for networking " />
+      </Head>
       <main>
         <EventList items={props.events} />
       </main>
